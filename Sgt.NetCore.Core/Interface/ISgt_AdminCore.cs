8b865b4aca31f5bcba5bccd1ab9e8a2522184f9a
xifucom <ylfan@live.com>
2016-12-13 15:20:34 +0800

    Sgt.NetCore
    This is a simple example, based on the.NetCore1.0 framework to write the
    WebAPI project. Project with Autofac, EF, Newtonsoft.Json, Repository
    and other technology to achieve. Can be released to the Linux production
    environment.

---
+++ Sgt.NetCore.Core/Interface/ISgt_AdminCore.cs
@@ -0,0 +1,14 @@
+﻿using Sgt.NetCore.Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Sgt.NetCore.Core.Interface
+{
+    public interface ISgt_AdminCore
+    {
+        Task<List<Sgt_Admin>> GetAllAsync(Expression<Func<Sgt_Admin, bool>> predicate);
+    }
+}
